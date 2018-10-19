bcda6c319817fd664f621aba36ca7d62bc44a0a2
decagondev <decagondevelopment@gmail.com>
2018-10-19 18:15:15 +0100

    (STRETCH) : add handleInhandleSunmit to SignUp component

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -12,6 +12,17 @@ class SignUp extends React.Component {
   handleInputChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3300/api/register", this.state)
+      .then(response => {
+        localStorage.setItem("jwt", response.data);
+        this.props.history.push("/api/jokes");
+      })
+      .catch(error => console.log(error));
+  };
 }
 
 export default SignUp;

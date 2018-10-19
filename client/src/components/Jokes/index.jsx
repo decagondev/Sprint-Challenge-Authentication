082986604282a2112a7ad4a0d15872d1fe4612da
decagondev <decagondevelopment@gmail.com>
2018-10-19 18:21:48 +0100

    (STRETCH) : add skeleton of Jokes component

---
+++ client/src/components/Jokes/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Styles/index.css";
+import axios from "axios";
+
+class Jokes extends React.Component {
+  state = {
+    jokes: []
+  };
+}
+
+export default Jokes;

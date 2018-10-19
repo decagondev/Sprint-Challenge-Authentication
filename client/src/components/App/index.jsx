89f12fa6a8f48f3a2de7acd0d8251bc40e2036f3
decagondev <decagondevelopment@gmail.com>
2018-10-19 18:27:57 +0100

    (STRETCH) : add routes to the app component

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "../Styles/index.css";
-import { withRouter } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
+import SignUp from "../SignUp";
+import SignIn from "../SignIn";
+import Jokes from "../Jokes";
 
-class App extends React.Component {}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Route path="/api/register" component={SignUp} />
+        <Route path="/api/login" component={SignIn} />
+        <Route path="/api/jokes" component={Jokes} />
+      </div>
+    );
+  }
+}
 
 export default withRouter(App);

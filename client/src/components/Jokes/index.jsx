c981f0b1a22033e986ab18ec0b1e8f3d5415da61
decagondev <decagondevelopment@gmail.com>
2018-10-19 18:23:25 +0100

    (STRETCH) : add component DidMount to Jokes component

--- client/src/components/Jokes/index.jsx
+++ client/src/components/Jokes/index.jsx
@@ -6,6 +6,15 @@ class Jokes extends React.Component {
   state = {
     jokes: []
   };
+
+  componentDidMount() {
+    const token = localStorage.getItem("jwt");
+    const requestOptions = { headers: { authorization: token } };
+    axios
+      .get(`http://localhost:3300/api/jokes`, requestOptions)
+      .then(response => this.setState({ jokes: response.data }))
+      .catch(error => console.log(error));
+  }
 }
 
 export default Jokes;

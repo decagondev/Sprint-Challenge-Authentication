479f53885ea5a5e32640dda4f962a9d601cb9c69
decagondev <decagondevelopment@gmail.com>
2018-10-19 17:04:56 +0100

    (MVP) : added tokenGeneration to the middlewares.js file

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,15 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const bcrypt = require("bcrypt");
+const { authenticate, tokenGeneration } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
-}
+// decided to do async await for this as I enjoy the elegance of the structure it brings
+function register(req, res) {}
 
 function login(req, res) {
   // implement user login
@@ -19,12 +18,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
